445ee917f8979c2550c1c62fbad2072260811c81
rj31portfolio <rjrajuali31@gmail.com>
2025-06-12 10:56:30 +0530

    make nav

---
+++ inc/header.php
@@ -0,0 +1,127 @@
+  <!-- Start Top Header Area -->
+        <div class="top-header-area bg-white style-two">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-5 col-md-6">
+                        <form class="top-header-search position-relative">
+                            <input type="text" class="input-search d-block w-100" placeholder="Search Products ...">
+                            <button type="submit" class="position-absolute bg-transparent p-0 border-0">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="col-lg-3 col-md-12 d-none d-lg-block">
+                        <div class="top-header-logo text-center">
+                            <a href="index.html" class="d-inline-block">
+                                <img src="assets/images/logo.png" alt="logo">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="top-header-info d-flex align-items-center justify-content-center justify-content-md-end">
+                            <div class="phone-number">
+                                <img src="assets/images/icons/support2.png" alt="support">
+                                <span class="d-block">
+                                    Call Us Free
+                                </span>
+                                <a href="tel:+91458654528">
+                                    +91 458 654 528
+                                </a>
+                            </div>
+                            <div class="user-btn">
+                                <a href="profile-authentication.html" class="d-inline-block">
+                                    <img src="assets/images/icons/user2.png" alt="user">
+                                </a>
+                            </div>
+                            <div class="cart-btn">
+                                <button type="button" class="d-inline-block cartToggleBtn">
+                                    <img src="assets/images/icons/bag2.png" alt="bag">
+                                    <span class="d-inline-block">
+                                        3
+                                    </span>
+                                </button>
+                                <div class="cart-box">
+                                    <div class="box d-flex align-items-center">
+                                        <div class="image">
+                                            <a href="product-details.html" class="d-block">
+                                                <img src="assets/images/products/product29.png" alt="product-image">
+                                            </a>
+                                        </div>
+                                        <div class="content">
+                                            <h4>
+                                                <a href="product-details.html">
+                                                    Frosted Flakes
+                                                </a>
+                                            </h4>
+                                            <div class="price">
+                                                $21.00 x 3
+                                            </div>
+                                        </div>
+                                        <button type="button">
+                                            <i class="fa-solid fa-xmark"></i>
+                                        </button>
+                                    </div>
+                                    <div class="box d-flex align-items-center">
+                                        <div class="image">
+                                            <a href="product-details.html" class="d-block">
+                                                <img src="assets/images/products/product30.png" alt="product-image">
+                                            </a>
+                                        </div>
+                                        <div class="content">
+                                            <h4>
+                                                <a href="product-details.html">
+                                                    Kids Dress
+                                                </a>
+                                            </h4>
+                                            <div class="price">
+                                                $21.00 x 1
+                                            </div>
+                                        </div>
+                                        <button type="button">
+                                            <i class="fa-solid fa-xmark"></i>
+                                        </button>
+                                    </div>
+                                    <div class="box d-flex align-items-center">
+                                        <div class="image">
+                                            <a href="product-details.html" class="d-block">
+                                                <img src="assets/images/products/product31.png" alt="product-image">
+                                            </a>
+                                        </div>
+                                        <div class="content">
+                                            <h4>
+                                                <a href="product-details.html">
+                                                    Organic Peanuts
+                                                </a>
+                                            </h4>
+                                            <div class="price">
+                                                $21.00 x 2
+                                            </div>
+                                        </div>
+                                        <button type="button">
+                                            <i class="fa-solid fa-xmark"></i>
+                                        </button>
+                                    </div>
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <span class="d-block fw-bold">
+                                            Subtotal:
+                                        </span>
+                                        <span class="d-block fw-bold">
+                                            $100.11
+                                        </span>
+                                    </div>
+                                    <div class="btn-box">
+                                        <button type="button" class="default-btn d-block w-100">
+                                            View Cart
+                                        </button>
+                                        <button type="button" class="default-btn d-block w-100">
+                                            Checkout
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Top Header Area -->
---
+++ inc/nav.php
@@ -0,0 +1,134 @@
+<!-- Start Navbar Area -->
+        <div class="navbar-area style-two">
+            <div class="kidsmart-responsive-nav">
+                <div class="container">
+                    <div class="kidsmart-responsive-menu">
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="assets/images/logo.png" alt="logo">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="kidsmart-nav">
+                <div class="container">
+                    <nav class="navbar navbar-expand-md navbar-light">
+                        <a class="navbar-brand d-none" href="index.html">
+                            <img src="assets/images/logo.png" alt="logo">
+                        </a>
+                        <div class="collapse navbar-collapse mean-menu">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a href="javascript:void(0);" class="dropdown-toggle nav-link active">Demos</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="index.html" class="nav-link">Home Demo - 1</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="index-2.html" class="nav-link">Home Demo - 2</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="index-3.html" class="nav-link">Home Demo - 3</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="index-4.html" class="nav-link">Home Demo - 4</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="index-5.html" class="nav-link active">Home Demo - 5</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="index-6.html" class="nav-link">Home Demo - 6</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="shop-grid.html" class="nav-link">Super Deals</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="javascript:void(0);" class="dropdown-toggle nav-link">Pages</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="about.html" class="nav-link">About Us</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link">Order Tracking</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link">My Account</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link">FAQ</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="dropdown-toggle nav-link">My Account</a>
+                                            <ul class="dropdown-menu">
+                                                <li class="nav-item">
+                                                    <a href="profile-authentication.html" class="nav-link">Login</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="profile-authentication.html" class="nav-link">Register</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="forgot-password.html" class="nav-link">Forgot Password</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link">Privacy Policy</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link">Terms & Conditions</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link">404 Error Page</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="javascript:void(0);" class="dropdown-toggle nav-link">Shop</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="shop-grid.html" class="nav-link">Shop Grid</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="shop-list.html" class="nav-link">Shop List</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="shop-grid-left-sidebar.html" class="nav-link">Shop Grid Left Sidebar</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="shop-list-left-sidebar.html" class="nav-link">Shop List Left Sidebar</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="shop-grid-right-sidebar.html" class="nav-link">Shop Grid Right Sidebar</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="shop-list-right-sidebar.html" class="nav-link">Shop List Right Sidebar</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="product-details.html" class="nav-link">Product Details</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="javascript:void(0);" class="dropdown-toggle nav-link">Blog</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="blog.html" class="nav-link">Blog</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="blog-details.html" class="nav-link">Blog Details</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="contact.html" class="nav-link">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- End Navbar Area -->
