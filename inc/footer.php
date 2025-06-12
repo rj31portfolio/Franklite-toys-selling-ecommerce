15f6637bc0b9ef74ecb4125bca7874300a80c194
rj31portfolio <rjrajuali31@gmail.com>
2025-06-12 11:13:02 +0530

    make strctures

---
+++ inc/footer.php
@@ -0,0 +1,178 @@
+   <!-- Start Footer Area -->
+        <footer class="footer-area pt-125">
+            <div class="container">
+                <div class="row" data-cues="slideInUp" data-group="footerContent">
+                    <div class="col-lg-4 col-sm-6">
+                        <div class="single-footer-widget">
+                            <a href="index.html" class="logo d-inline-block">
+                                <img src="assets/images/white-logo.png" alt="white-logo">
+                            </a>
+                            <p>
+                                We are committed to providing the highest-quality products that you can trust.
+                            </p>
+                            <div class="info">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <img src="assets/images/icons/icon4.png" alt="icon">
+                                    </div>
+                                    <span>HotLine Order</span>
+                                </div>
+                                <a href="tel:(012)3456789">(012) 345 6789</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-6">
+                        <div class="single-footer-widget">
+                            <h4>
+                                Useful links 
+                            </h4>
+                            <ul class="ps-0 mb-0 list-unstyled custom-links">
+                                <li>
+                                    <a href="#">
+                                        New Products
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Best Sellers
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Bundle & Save
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Online Gift Card
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Discount
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-6">
+                        <div class="single-footer-widget">
+                            <h4>
+                                My account
+                            </h4>
+                            <ul class="ps-0 mb-0 list-unstyled custom-links">
+                                <li>
+                                    <a href="#">
+                                        My Profile
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        My Order History
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        My Wishlist
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Order Tracking
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Shopping Cart
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <div class="single-footer-widget">
+                            <h4>
+                                Sign Up for Our Newsletter
+                            </h4>
+                            <p>
+                                Leave your email to get all hot deals & news which benefit you most! 
+                            </p>
+                            <form>
+                                <div class="icon">
+                                    <i class="fa-regular fa-envelope"></i>
+                                </div>
+                                <input type="text" class="input-email d-block w-100 border-0" placeholder="Your email">
+                                <button type="submit">
+                                    <i class="fa-regular fa-paper-plane"></i>
+                                </button>
+                            </form>
+                            <div class="social-links">
+                                <a href="#" target="_blank" class="d-inline-block facebook">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </a>
+                                <a href="#" target="_blank" class="d-inline-block twitter">
+                                    <i class="fa-brands fa-twitter"></i>
+                                </a>
+                                <a href="#" target="_blank" class="d-inline-block linkedin">
+                                    <i class="fa-brands fa-linkedin-in"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-area">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4">
+                            <p>
+                                © 2023 <span>KidsMart</span>. All Rights Reserved
+                            </p>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="payment-methods">
+                                <img src="assets/images/payment-methods/visa-electron.png" alt="visa-electron">
+                                <img src="assets/images/payment-methods/visa.png" alt="visa">
+                                <img src="assets/images/payment-methods/master-card.png" alt="master-card">
+                                <img src="assets/images/payment-methods/american-express.png" alt="american-express">
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul class="ps-0 mb-0 list-unstyled">
+                                <li class="d-inline-block position-relative">
+                                    <a href="#">
+                                        Privacy & Cookie Policy
+                                    </a>
+                                </li>
+                                <li class="d-inline-block position-relative">
+                                    <a href="#">
+                                        Terms Of Services
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End Footer Area -->
+
+        <!-- Go Top -->
+        <div class="go-top">
+            <i class="fa-solid fa-chevron-up"></i>
+        </div>
+
+        <!-- Link of JS Files -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/jquery-ui.min.js"></script>
+        <script src="assets/js/mixitup.min.js"></script>
+        <script src="assets/js/meanmenu.min.js"></script>
+        <script src="assets/js/scrollCue.min.js"></script>
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <script src="assets/js/slick.min.js"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="zxx">
+    
+<!-- Mirrored from thepixelcurve.com/html/kidsmart/kidsmart/index-5.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 12 Jun 2025 04:10:38 GMT -->
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Link of CSS Files -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="assets/css/scrollCue.min.css">
+        <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+        <link rel="stylesheet" href="assets/css/slick.css">
+        <link rel="stylesheet" href="assets/css/all.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+
+        <title>KidsMart - Baby & Kids Store eCommerce HTML Template</title>
+        <link rel="icon" type="image/png" href="assets/images/favicon.png">
+    </head>
+    <body>
+        <!--====== PRELOADER PART START ======-->
+        <div id="preloader">
+            <div class="preloader">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <!--====== PRELOADER PART ENDS ======-->
+
+        <!-- Start Top Header Area -->
+        
+        <!-- End Top Header Area -->
+
+        <!-- Start Navbar Area -->
+       
+        <!-- End Navbar Area -->
+
+        <!-- Start Mega Kids Banner Area -->
+        
+        <!-- End Mega Kids Banner Area -->
+
+        <!-- Start Features Area -->
+       
+        <!-- End Features Area -->
+
+        <!-- Start Featured Categories Area -->
+     
+        <!-- End Featured Categories Area -->
+
+        <!-- Start Featured Products Area -->
+       
+        <!-- End Featured Products Area -->
+
+        <!-- Start Categories Area -->
+       
+        <!-- End Categories Area -->
+
+        <!-- Start Offer Area -->
+       
+        <!-- End Offer Area -->
+
+        <!-- Start Most Popular & Top Rated Products Area -->
+        
+        <!-- End Most Popular & Top Rated Products Area -->
+
+        <!-- Start Brands Area -->
+    
+        <!-- End Brands Area -->
+
+        <!-- Start Product Quickview Modal -->
+       
+        <!-- End Product Quickview Modal -->
+
+        <!-- Start Footer Area -->
+
+        <!-- Footer Area end -->
+       
